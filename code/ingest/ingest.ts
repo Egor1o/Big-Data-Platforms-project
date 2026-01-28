f7d58fb93b5c0e03a499eb82ad98b07c534df30c
Egor1o <egorklo147@gmail.com>
2026-01-29 00:47:39 +0200

    added typescript config and basic script for ingestion

---
+++ code/ingest/ingest.ts
@@ -0,0 +1,45 @@
+import Database from 'better-sqlite3';
+
+const db = new Database("../../tenant/database.sqlite", { readonly: true });
+
+const stmt = db.prepare(`
+  SELECT *
+  FROM May2015
+  WHERE created_utc BETWEEN ? AND ?
+  ORDER BY created_utc
+`);
+
+const BATCH_SIZE = 500;
+let batch: any[] = [];
+
+const flushBatch = (batch: any[]) => {
+    batch.splice(0,batch.length)
+}
+
+function ingestData(rangeStart:number, rangeEnd:number) {
+    for (const row of stmt.iterate(rangeStart, rangeEnd)) {
+
+        console.log(row);
+        batch.push(row);
+
+        if (batch.length >= BATCH_SIZE) {
+            flushBatch(batch);
+            batch.length = 0;
+        }
+    }
+
+    if (batch.length > 0) {
+        flushBatch(batch);
+    }
+
+    db.close();
+}
+
+const RANGE_START = 1420070400;
+const RANGE_END = 1433116800;
+
+ingestData(RANGE_START, RANGE_END);
+
+
+
+
