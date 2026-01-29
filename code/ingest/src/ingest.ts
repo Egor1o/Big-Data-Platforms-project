c9e1a6f621ed4eb55d9f0301027e159345452ce6
Egor1o <egorklo147@gmail.com>
2026-01-30 00:26:44 +0200

    Tested connection, fixed db connection link.

--- code/ingest/src/ingest.ts
+++ code/ingest/src/ingest.ts
@@ -1,5 +1,6 @@
 import Database from 'better-sqlite3';
 import {calculateRange} from "./utils.js";
+import {Client} from "pg";
 const db = new Database("../../tenant/database.sqlite", { readonly: true });
 
 const stmt = db.prepare(`
@@ -12,18 +13,31 @@ const stmt = db.prepare(`
 const BATCH_SIZE = 500;
 let batch: any[] = [];
 
+const client = new Client({
+    connectionString: process.env.DATABASE_URL
+})
+
+await client.connect()
+
 const flushBatch = (batch: any[]) => {
     batch.splice(0,batch.length)
 }
 
-function ingestData(rangeStart:number, rangeEnd:number) {
+async function ingestData(rangeStart:number, rangeEnd:number) {
     for (const row of stmt.iterate(rangeStart, rangeEnd)) {
 
-        console.log(row);
+        //console.log(row);
         batch.push(row);
 
         if (batch.length >= BATCH_SIZE) {
             flushBatch(batch);
+            const res = await client.query(`
+              SELECT id, author, subreddit, created_utc
+              FROM comments
+              ORDER BY created_utc
+              LIMIT 5
+            `);
+            console.log('the result', res.rows);
             batch.length = 0;
         }
     }
@@ -44,7 +58,7 @@ if(!workerIdEnv) throw new Error("WORKER_ID env var not set");
 const workerId = parseInt(workerIdEnv)
 const {rangeStart, rangeEnd} = calculateRange(workerId, workersTotal);
 
-ingestData(rangeStart, rangeEnd);
+await ingestData(rangeStart, rangeEnd);
 
 
 
--- code/migrations/V1__initial.sql
+++ code/migrations/V1__initial.sql
@@ -2,33 +2,33 @@ CREATE DATABASE IF NOT EXISTS reddit;
 USE reddit;
 
 CREATE TABLE IF NOT EXISTS comments (
-                                        id STRING PRIMARY KEY,
+    id STRING PRIMARY KEY,
 
-                                        name STRING,
-                                        link_id STRING,
-                                        parent_id STRING,
-                                        subreddit_id STRING,
-                                        subreddit STRING,
+    name STRING,
+    link_id STRING,
+    parent_id STRING,
+    subreddit_id STRING,
+    subreddit STRING,
 
-                                        author STRING,
-                                        author_flair_text STRING,
-                                        author_flair_css_class STRING,
+    author STRING,
+    author_flair_text STRING,
+    author_flair_css_class STRING,
 
-                                        body STRING,
+    body STRING,
 
-                                        created_utc INT,
-                                        retrieved_on INT,
+    created_utc INT,
+    retrieved_on INT,
 
-                                        ups INT,
-                                        downs INT,
-                                        score INT,
-                                        score_hidden BOOL,
+    ups INT,
+    downs INT,
+    score INT,
+    score_hidden BOOL,
 
-                                        gilded INT,
-                                        archived BOOL,
-                                        edited BOOL,
-                                        controversiality INT,
+    gilded INT,
+    archived BOOL,
+    edited BOOL,
+    controversiality INT,
 
-                                        distinguished STRING,
-                                        removal_reason STRING
+    distinguished STRING,
+    removal_reason STRING
 );
